5131ffc3df390cfb50ee74bbda3de529e48e3a25
cushitrealgood <cushenberry.adrian@gmail.com>
2020-03-02 19:03:14 +0000

    cleanup and improved readme

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM codercom/code-server
 
-#Install Terraform
-FROM codercom/code-server:latest
 RUN sudo apt-get install -y unzip
+
+#Install Terraform
 ADD terraform.sh /
 RUN sudo chmod +x /terraform.sh
 RUN sudo /terraform.sh
--- terraform.sh
+++ terraform.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
 sudo apt-get install -y jq 
-CURRR_VER=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')
-CURRR_VER=$CURRR_VER
-wget "https://releases.hashicorp.com/terraform/${CURRR_VER}/terraform_${CURRR_VER}_linux_amd64.zip"
-unzip terraform_${CURRR_VER}_linux_amd64.zip
-sudo rm terraform_${CURRR_VER}_linux_amd64.zip
+CUR_VER=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')
+wget "https://releases.hashicorp.com/terraform/${CUR_VER}/terraform_${CUR_VER}_linux_amd64.zip"
+unzip terraform_${CUR_VER}_linux_amd64.zip
+sudo rm terraform_${CUR_VER}_linux_amd64.zip
 sudo mv terraform /usr/local/bin/terraform
 sudo chmod +x /usr/local/bin/terraform
